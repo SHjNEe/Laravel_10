f0c5f26ecaa045f463dd038a7ef89ed89a189299
SHjNEe <baotrung.jpvn@gmail.com>
2023-08-06 21:41:09 +0700

    Add sanctum

--- event-management/routes/api.php
+++ event-management/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AttendeeController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::apiResource('events', EventController::class);
