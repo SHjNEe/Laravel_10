93bde060b4169668b67db414161aa58af0ad22b0
SHjNEe <baotrung.jpvn@gmail.com>
2023-08-09 22:34:41 +0700

    Config broadcast

--- real-time-course/config/broadcasting.php
+++ real-time-course/config/broadcasting.php
@@ -37,11 +37,17 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                // 'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
+                // 'port' => env('PUSHER_PORT', 443),
+                // 'scheme' => env('PUSHER_SCHEME', 'https'),
+                // 'encrypted' => true,
+                // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
+                'port' => 6001,
+                'scheme' => 'http',
+                // 'encrypted' => true,
+                'useTLS' => true,
+
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
